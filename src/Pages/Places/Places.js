7157845c321b7b9725d5a1498babfee2524509c1
Developer <error@Error.error>
2022-12-10 00:26:05 +0600

    book

--- src/Pages/Places/Places.js
+++ src/Pages/Places/Places.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../Context/UserContext/UserContext';
 
 
 const Places = () => {
 
+    const { user } = useContext(AuthContext);
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const [dat, setData] = useState();
     useEffect(() => {
         fetch(`http://localhost:5000/tours/${localStorage.getItem('Id')}`)
@@ -13,6 +17,31 @@ const Places = () => {
                 setData(data);
             })
     })
+
+    const handleLogin = event => {
+        const name = user?.displayName;
+        const email = user?.email;
+        const number = event.number;
+        const message = event.message;
+        const title = dat?.title;
+
+        console.log(name, email, number, message);
+        const newData = { name, email, number, message, title };
+
+
+        fetch('http://localhost:5000/booking', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newData)
+        })
+            .then(res => res.json())
+            .then(data => {
+            })
+            .catch(error => console.log(error));
+
+    }
 
     return (
         <Container className='py-5'>
@@ -47,6 +76,27 @@ const Places = () => {
 
                         </Card.Body>
                     </Card>
+
+                    <Form onSubmit={handleSubmit(handleLogin)} className='text-start'>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Name</Form.Label>
+                            <input disabled defaultValue={user?.displayName} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <input disabled defaultValue={user?.email} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Phone Number</Form.Label>
+                            <input {...register("number", { required: "Number is required", minLength: "11" })} placeholder='01xxxxxxxx' type="tel" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            {errors.number?.type === 'required' && <p className='text-red-700' role="alert">Email is required</p>}
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Message</Form.Label>
+                            <textarea {...register("message")} placeholder='Type Your message' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </Form.Group>
+                        <input className='btn btn-primary w-100' type="submit" />
+                    </Form>
                 </Col>
                 <Col>
                     <p>Book Options</p>
