06c295e64c08b0443ed97ab385cb08df862e94fa
Developer <error@Error.error>
2022-12-13 23:54:11 +0600

    dec 11.54

--- src/Pages/Home/ClientReview/ClientReview.js
+++ src/Pages/Home/ClientReview/ClientReview.js
@@ -3,7 +3,7 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const ClientReview = () => {
     const [reviews, setReview] = useState([]);
-    const BgImg = "https://images.unsplash.com/photo-1669839556036-ba8316adba5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+
 
     useEffect(() => {
         fetch(`http://localhost:5000/reviews`)
@@ -21,10 +21,10 @@ const ClientReview = () => {
                         <Card style={{ width: '18rem' }} className="text-start">
                             <div className='card-body'>
                                 <div className='text-center'>
-                                    <img className='img-fluid w-25 rounded-circle' src={review?.imgUrl} alt="" />
-                                    <h6 className="card-title mt-2">Name:{review?.name}</h6>
+                                    <img className='img-fluid w-25 rounded-circle' src={review.imgUrl} alt="" />
+                                    <h6 className="card-title mt-2">Name:{review.name}</h6>
                                 </div>
-                                <p className="card-text my-3">Review:{review?.review}</p>
+                                <p className="card-text my-3">Review:{review.review}</p>
                             </div>
                         </Card>
                     </Col>)
