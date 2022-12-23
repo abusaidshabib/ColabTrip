35f04ff054e98e4025e3b7f1e1e4659331d6b1eb
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-12-23 21:39:39 +0600

    Border added

--- src/Pages/Home/RunningTour/RunningTour.js
+++ src/Pages/Home/RunningTour/RunningTour.js
@@ -19,25 +19,27 @@ const RunningTour = () => {
     return (
         <Container className='py-5'>
             <p className='text-uppercase display-4 fw-bold'>Trending Tour</p>
-            <Row xs={1} md={2} lg={3} className="g-5 m-0 py-5">
+            <div className="row m-0 g-5 py-5">
                 {
-                    tours.map(tour => <span className="">
-                        <div className='w-100' style={{ height: '22rem' }}>
-                            <Card.Img variant="top" className='h-100 rounded-top' style={{
-                                objectFit: 'cover',
-                                position: 'center'
-                            }}
-                                src={tour.img} />
+                    tours.map(tour => <div className="col-12 col-md-6 col-lg-4">
+                        <div className='border'>
+                            <div className='w-100' style={{ height: '22rem' }}>
+                                <Card.Img variant="top" className='h-100 rounded-top' style={{
+                                    objectFit: 'cover',
+                                    position: 'center'
+                                }}
+                                    src={tour.img} />
+                            </div>
+                            <div className="card-body py-3">
+                                <p className='fs-3 fw-bold'>{tour.title}</p>
+                                <p className='fw-semibold fs-6'>Duration: {tour.duration}</p>
+                                <p className='fw-semibold fs-6'>Fee: {tour.fee}</p>
+                                <Link onClick={() => getId(`${tour._id}`)} to={`/places/${tour._id}`} className='btn btn-light text-uppercase fw-semibold'>Book Now</Link>
+                            </div>
                         </div>
-                        <div className="card-body">
-                            <p className='display-6 fw-bold'>{tour.title}</p>
-                            <p className='fw-semibold fs-6'>Duration: {tour.duration}</p>
-                            <p className='fw-semibold fs-6'>Fee: {tour.fee}</p>
-                            <Link onClick={() => getId(`${tour._id}`)} to={`/places/${tour._id}`} className='btn btn-light text-uppercase fw-semibold'>Book Now</Link>
-                        </div>
-                    </span>)
+                    </div>)
                 }
-            </Row>
+            </div>
             <Link className='btn btn-outline-dark text-uppercase border-2' to='/trending'>See All Places</Link>
         </Container>
     );
